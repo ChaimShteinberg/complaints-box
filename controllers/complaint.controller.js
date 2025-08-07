a560ddd92291b3d106f31d4b6995358d9027d392
Chaim Shteinberg <a0533197133@gmail.com>
2025-08-07 16:12:53 +0300

    get all controller

--- controllers/complaint.controller.js
+++ controllers/complaint.controller.js
@@ -1,4 +1,4 @@
-import db from "../db/connect.db";
+import db from "../db/connect.db.js";
 
 const collection = db.collection("complaints");
 
@@ -7,6 +7,15 @@ export async function submitComplaint(req, res) {
     const { category, message } = req.body;
     await collection.insertOne({ category, message });
     res.send("The form was submitted successfully.");
+  } catch (error) {
+    res.status(error.status | 400).send(error.message);
+  }
+}
+
+export async function getAllComplaints(req, res) {
+  try {
+    const complaints = await collection.find().toArray();
+    res.json(complaints);
   } catch (error) {
     res.status(error.status | 400).send(error.message);
   }
--- routes/complaint.route.js
+++ routes/complaint.route.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { submitComplaint } from "../controllers/complaint.controller.js";
+import {
+  getAllComplaints,
+  submitComplaint,
+} from "../controllers/complaint.controller.js";
 
 const router = express.Router();
 
 router.post("/submit", submitComplaint);
 
-router.get("/getAll");
+router.get("/getAll", getAllComplaints);
 
 export default router;
