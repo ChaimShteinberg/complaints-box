b2d526838de08389068a6cd84eaf1583aca53bfd
Chaim Shteinberg <a0533197133@gmail.com>
2025-08-07 15:12:42 +0300

    start server

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+import express from "express";
 import db, { connectToMongo } from "./db/connect.db.js";
 
+const app = express();
+
 await connectToMongo();
+
+app.listen(process.env.PORT, console.log("listennung..."));
